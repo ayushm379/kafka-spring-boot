8f05cb7c004908e4d21b262bb61d2a6eb2d6d989
Ayush Tewari <ayushtewari379@gmail.com>
2025-07-24 04:17:03 +0530

    Added Lesson 01

---
+++ src/main/java/io/at/learn/kafka/lesson01/consumer/KafkaConsumer01Config.java
@@ -0,0 +1,36 @@
+package io.at.learn.kafka.lesson01.consumer;
+
+import io.at.learn.kafka.lesson01.producer.KafkaProducerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumer01Config {
+
+    public final static String GROUP_ID = "group_l01_01";
+
+    @Bean(name = "consumer_factory_l01_01")
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaProducerConfig.BOOTSTRAP_SERVER,
+                ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID,
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class
+        ));
+    }
+
+    @Bean(name = "Kafka_Listener_l01_01")
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+}
---
+++ src/main/java/io/at/learn/kafka/lesson01/producer/KafkaProducerConfig.java
@@ -0,0 +1,33 @@
+package io.at.learn.kafka.lesson01.producer;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    public final static String TOPIC_NAME = "lesson01";
+    public final static String BOOTSTRAP_SERVER = "localhost:90902";
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER,
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        ));
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
